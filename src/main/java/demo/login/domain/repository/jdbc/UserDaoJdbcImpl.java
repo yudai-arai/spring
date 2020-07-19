0e127afe8993e2c94601534e6054245124e7b28b
yudai-arai <glass-turcky@i.softbank.jp>
2020-07-19 23:39:18 +0900

    複数件取得

--- src/main/java/demo/login/domain/repository/jdbc/UserDaoJdbcImpl.java
+++ src/main/java/demo/login/domain/repository/jdbc/UserDaoJdbcImpl.java
@@ -7,7 +7,10 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class UserDaoJdbcImpl implements UserDao {
@@ -16,12 +19,29 @@ public class UserDaoJdbcImpl implements UserDao {
 
     @Override
     public int count() throws DataAccessException {
-        return 0;
+        int count = jdbc.queryForObject("SELECT COUNT(*)FROM m_user", Integer.class);
+        return count;
     }
 
     @Override
     public int insertOne(User user) throws DataAccessException {
-        return 0;
+        int rowNumber = jdbc.update("INSERT INTO m_user(user_id,"
+        + "password,"
+        + "user_name,"
+        + "birthday,"
+        + "age,"
+        + "marriage,"
+        + "role)"
+        + "VALUES(?,?,?,?,?,?,?)"
+        , user.getUserId()
+        , user.getPassword()
+        , user.getUserName()
+        , user.getBirthday()
+        , user.getAge()
+        , user.isMarriage()
+        , user.getRole());
+
+        return rowNumber;
     }
 
     @Override
@@ -31,7 +51,24 @@ public class UserDaoJdbcImpl implements UserDao {
 
     @Override
     public List<User> selectMany() throws DataAccessException {
-        return null;
+        List<Map<String, Object>> getList = jdbc.queryForList("SELECT * FROM m_user");
+
+        List<User> userList = new ArrayList<>();
+
+        for(Map<String, Object> map:getList) {
+            User user = new User();
+
+            user.setUserId((String)map.get("user_id"));
+            user.setPassword((String)map.get("password"));
+            user.setUserName((String)map.get("user_name"));
+            user.setBirthday((Date)map.get("birthday"));
+            user.setAge((Integer)map.get("age"));
+            user.setMarriage((Boolean)map.get("marriage"));
+            user.setRole((String)map.get("role"));
+
+            userList.add(user);
+        }
+        return userList;
     }
 
     @Override
